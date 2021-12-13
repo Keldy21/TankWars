79134fb7f812a486529034fb254653d56b2f195e
Keldy21 <Keldy21@live.co.uk>
2021-12-13 12:30:57 +0000

    Morning
    Started to create base tank as well as created the tiled map

---
+++ js/BaseTank.js
@@ -0,0 +1,31 @@
+class BaseTank {
+    /** @type {number}*/
+    tankSpeed = 100
+
+    /** @type {number} */
+    damageMax = 2
+    constructor(scene, x, y, texture, frame){
+
+    }
+    update(){
+
+    }
+    damage(){
+
+    }
+    setBullets(){
+
+    }
+    burn(){
+
+    }
+    isDestroyed(){
+
+    }
+    isImmobilise(){
+
+    }
+    enableCollision(){
+
+    }
+}
